f08b609dbb1b7d80ddd27e0e086196699ca6e74d
dscolari-prog <dscolari@remax.com.ar>
2026-04-27 13:01:26 -0300

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -241,4 +241,5 @@ def api_stats():
     return jsonify(stats)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+    app.run(host="0.0.0.port = int(os.environ.get("PORT", 8080))
+app.run(host="0.0.0.0", port=port, debug=False)0", port=int(os.environ.get("PORT", 5000)), debug=False)
